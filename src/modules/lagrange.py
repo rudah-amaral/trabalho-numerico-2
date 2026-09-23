345641eac901c71d70228084c2e934813aef1d1f
Samuel Pinagé <samuelson1345@gmail.com>
2026-09-22 23:37:07 -0300

    Desvio relativo percentual e alguns comentarios

--- src/modules/lagrange.py
+++ src/modules/lagrange.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass
 
+#Vamos ter que criar um função para interpretar funções (já temos isso pronto)
+#para ficar mais dinamico, só do hermes colocar o x_alvo e a função o nosso programa vai saber a imagem desejada
+#lagrange funciona assim??? (não lembro direito)
+#para não correr nenhum risco de perder valores importantes, a gnt vai precisar ser bem redundante
+#amanhã eu vou fazer o neville
+
 @dataclass
 class Ponto:
     x: int # coordenada no eixo x
@@ -17,5 +23,20 @@ def interpolar_dois_pontos(pontos: list[Ponto], x_alvo) -> list[float]:
                 valores.append(p)
 
     return valores
+
+
+def neville(ponto: list[Ponto], x_alvo) -> list[float]:
+
+
+def desvio_relativo_percentual(x_alvo, resultados: list[float]) -> list[float]:
+    n = len(resultados)
+    DRP = []
+
+    for i in range(n):
+        Desvio = abs((resultados[i] - x_alvo) / x_alvo)
+        DRP.append(Desvio)
+
+    return DRP
+
 
 
